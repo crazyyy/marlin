45fe6f8ceeb4b870916eaf9625601ae006422cee
crazyyy <crazyyy@gmail.com>
2016-04-20 19:45:47 +0300

    prerelise

--- wordpress/wp-content/themes/wp-marlin/footer.php
+++ wordpress/wp-content/themes/wp-marlin/footer.php
@@ -7,11 +7,11 @@
 <footer role="contentinfo">
   <div class="container">
     <div class="row">
-      <div class="col-md-4 footer-copyright">
+      <div class="col-md-3 col-sm-6 col-xs-6 footer-copyright">
         <p><?php bloginfo('name'); ?><br><?php _e( 'ALL RIGHTS RESERVED', 'wpeasy' ); ?><br>2015-<?php echo date("Y"); ?> <?php _e( 'year', 'wpeasy' ); ?></p>
       </div><!-- /.col-md-2 footer-copyright -->
 
-      <div class="col-md-4 col-md-offset-4 footer-phone">
+      <div class="col-md-4 col-md-offset-5 col-sm-6 col-xs-6 footer-phone">
         <a href="mailto:">marlin.burgas@gmail.com</a>
         <a href="phone:">+359 877 255 229</a>
         <a href="phone:">+359 877 255 224</a>
